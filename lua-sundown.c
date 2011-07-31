2408cee850335d87738a515dd37dae9ef30792b3
Adrian Sampson <adrian@radbox.org>
2011-07-31 11:50:57 -0700

    add SmartyPants pass

--- lua-sundown.c
+++ lua-sundown.c
@@ -13,26 +13,33 @@ static int sundown_markdown(lua_State *L) {
     const char *indoc;
     size_t indocSize;
     const char *outdoc;
-    struct buf inbuf, *outbuf;
+    struct buf inbuf, *outbuf, *spbuf;
     struct mkd_renderer renderer;
+    int targetSize;
 
     indoc = luaL_checklstring(L, 1, &indocSize);
     memset(&inbuf, 0x0, sizeof(struct buf));   
     inbuf.data = indoc;
     inbuf.size = indocSize;
 
-    // 20% HTML overhead guess inspired by Redcarpet.
+    // Overhead guess inspired by Redcarpet.
+    targetSize = (int)indocSize * 1.3f;
     outbuf = bufnew(128);
-    bufgrow(outbuf, indocSize * 1.2f);
+    bufgrow(outbuf, targetSize);
 
     sdhtml_renderer(&renderer, 0, NULL);
     sd_markdown(outbuf, &inbuf, &renderer, 0);
 
-    //FIXME smartypants?
+    // Smartypants -- should maybe make this optional, but it's
+    // standard in most Markdown libraries so it's the default.
+    spbuf = bufnew(128);
+    bufgrow(spbuf, targetSize);
+    sdhtml_smartypants(spbuf, outbuf);
 
-    lua_pushlstring(L, outbuf->data, outbuf->size);
+    lua_pushlstring(L, spbuf->data, spbuf->size);
     
     bufrelease(outbuf);
+    bufrelease(spbuf);
     sdhtml_free_renderer(&renderer);
     
     return 1;
